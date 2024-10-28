d2e3cfab52cdbe8691c12f59dcefe02751b14862
王志芳 <wangzhifang@wangzhifangdeMacBook-Air.local>
2024-10-29 01:11:03 +0800

    upd

--- c.hw2/MemAddress.c
+++ c.hw2/MemAddress.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 int main(){
 	int x;
-	printf("x is allocated at memory address %#1x-%#1x.\n",&x, (unsigned long)(&x)+sizeof(x)-1);
+	printf("x is allocated at memory address %p-%p.\n", (unsigned long)(&x)+sizeof(x)-1);
 }
