d196d22494d82a7d8eb9c11f7ffa40ea03e7e43b
王志芳 <wangzhifang@wangzhifangdeMacBook-Air.local>
2024-11-09 14:43:30 +0800

    UPD

--- c.hw4/IsPowerOf2.c
+++ c.hw4/IsPowerOf2.c
@@ -1,11 +1,18 @@
-include<stdio.h>
-int main(){
-	int n;
+#include <stdio.h>
+
+int main() {
 	printf("Please input an integer: ");
+	int n;
 	scanf("%d", &n);
-	if(n <= 0){
+	if(n <= 0) {
 		printf("Input error!\n");
 		return 0;
 	}
-	
+	for(int i=0; i<15; i++) {
+		if(n == (1<<i)) {
+			printf("%d is 2 to the power of %d.", n, i);
+			return 0;
+		}
+	}
+	printf("%d is not a power of 2.\n", n);
 }
